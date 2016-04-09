b4ca563760bc6c8e954d26a809f914de3c1edef7
Minh <MinhHNguyen95@gmail.com>
2016-04-09 15:33:25 -0700

    Fixed formatting

--- extension/chromeBookmarks.js
+++ extension/chromeBookmarks.js
@@ -1,17 +1,55 @@
-var processNode = function(node) {
-    // recursively process child nodes
+var levelToText = function(level) {
+	var value = "";
+
+	for(i = 1; i < level; i++) 
+		value += "|";
+
+	if(value)
+		value += " ";
+
+	return value;
+};
+
+var processNode = function(node, level) {
+	var spacing = levelToText(level);
+
+	// print folder name
+	if(!node.url && node.title) {
+	    	htmlBody += "<h2>"+spacing+node.title+"</h2>";
+    }
+
     if(node.children) {
-        node.children.forEach(function(child) { processNode(child); });
-    }
+	    var folders = [];
 
-    // print leaf nodes URLs to console
-    if(node.url) {
-		document.getElementsByTagName('body')[0].innerHTML += ("<a target='_blank' href=" + node.url + ">"+ node.url+"<\p>"); 
-    }
-}
+		node.children.forEach(function(child) {
+        	if(child.url) { // print leaf nodes' URLs (folder content)
+				htmlBody += "<p><a target='_blank' href="+child.url+">"
+							+spacing+child.title+"</a></p>";
+			} else { // otherwise it is a folder. add folder to queue
+				folders.push(child);
+			}
+        });
 
+	    // process the rest of the nodes (folders) in the queue
+	    folders.forEach(function(child) {
+	    	processNode(child, level+1);
+	    });
+    }
+};
+
+var htmlBody;
 chrome.bookmarks.getTree(function(itemTree){
+	htmlBody = "";
+
+	if(itemTree) {
+    	htmlBody += "<h1>Bookmarks Found, '|' indicates folder nesting</h1>";		
+	} else {
+    	htmlBody += "<h1>No Bookmarks Found</h1>";		
+	}
+    
     itemTree.forEach(function(item){
-        processNode(item);
+		processNode(item, 0);
     });
+
+    document.getElementsByTagName('body')[0].innerHTML += htmlBody;
 });
